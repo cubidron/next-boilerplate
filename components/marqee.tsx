214633c68905b0c481bbbf8faf948b3861ee43a4
Haume0 <haume341@outlook.com>
2026-03-24 12:54:59 +0300

    Update marqee.tsx

--- components/marqee.tsx
+++ components/marqee.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import "@/components/marqee.css";
 
 export default function Marquee(props: {
     title?: string;
@@ -7,36 +8,41 @@ export default function Marquee(props: {
 }) {
     return (
         <section
-            className={`container flex flex-col mx-auto group items-center ${
+            className={`relative flex flex-col mx-auto py-12 group items-center ${
                 props.className || ""
             }`}
         >
             <div className="absolute inset-0 size-full overflow-hidden">
-                <div className="size-full bg-gradient-to-t from-body/0 to-body/20 not-group-hover:opacity-0 not-group-hover:-translate-y-12 not-group-hover:blur-xl ease-in-out duration-1000" />
+                <div className="size-full bg-linear-to-t from-body/0 to-body/20 not-group-hover:opacity-0 not-group-hover:-translate-y-12 not-group-hover:blur-xl ease-in-out duration-1000" />
             </div>
             {props.title && (
-                <p className="font-medium text-body mb-4 text-center lg:text-start text-base lg:text-xl">
+                <p className="font-light text-white mb-4 text-center lg:text-start text-base lg:text-xl">
                     {props.title}
                 </p>
             )}
             <div
                 className="slider"
                 style={{
-                    ["--width" as any]: "10rem",
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    ["--width" as any]: `${props.items.length * 1}rem`,
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
                     ["--height" as any]: "8rem",
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
                     ["--quantity" as any]: props.items.length,
-                    ["--duration" as any]: "24s",
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    ["--duration" as any]: `${props.items.length * 2}s`,
                 }}
             >
                 <div className="list">
                     {props.items.map((item, i) => (
                         <Link
-                            target="_blank"
+                            target={item.url ? "_blank" : "_self"}
                             rel="noopener noreferrer"
                             href={item.url}
                             title={item.name}
                             key={i}
                             className="item"
+                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
                             style={{ ["--position" as any]: i }}
                         >
                             <img src={item.image} alt={`logo ${i + 1}`} />
